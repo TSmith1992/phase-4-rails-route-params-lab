b6672fb794d5770e00f7fddaa25e978d548742dd
Tarique Smith <tpsmith1992@gmail.com>
2021-09-20 11:50:47 -0400

    commit tree 4ef69784dc144916d395395ad987661c02d1edb1

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,13 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    # byebug
+    students = Student.all.find_by_first_name(params[:key1])
     render json: students
   end
 
+  def show
+    student=Student.find(params[:id])
+    render json: student
+  end
 end
